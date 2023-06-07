2982cb4c4b960328164b2455dcb2ea9a8dfd3e9d
Alex Currie-Clark <acurrieclark@gmail.com>
2023-06-07 15:56:13 +0100

    Release v0.4.3

--- src/filter.ts
+++ src/filter.ts
@@ -3,17 +3,17 @@
 /**
  * equivalent to `arr.filter()` but in-place
  */
-export function filter<T>(array: Array<T>, condition: (value: T, index: number) => boolean): T[]
-{
+export function filter<T>(
+  array: Array<T>,
+  condition: (value: T, index: number) => boolean,
+): T[] {
   let nextPlace = 0;
-  
-  for (const [index, value] of array.entries())
-  {
-    if (condition(value, index))
-      array[nextPlace++] = value;
+
+  for (const [index, value] of array.entries()) {
+    if (condition(value, index)) array[nextPlace++] = value;
   }
-  
+
   array.splice(nextPlace);
-  
+
   return array;
 }
